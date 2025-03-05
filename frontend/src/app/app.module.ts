1f9779852e9028b278edcf666d485e2e2b6a1526
mahmoud <mahmoud.chouchane@gmail.com>
2025-03-05 17:53:48 +0100

    first comm

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroSectionComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutComponent,
+    LandingComponent,
+    SidebarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
