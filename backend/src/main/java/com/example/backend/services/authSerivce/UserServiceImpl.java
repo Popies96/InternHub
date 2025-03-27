e318dc53454202879e94e1fec28f430b60dc58b4
Mohamed Aziz Belfaidi <azizbelfaidi.aziz@gmail.com>
2025-03-27 02:42:55 +0000

    user in angular done

--- backend/src/main/java/com/example/backend/services/authSerivce/UserServiceImpl.java
+++ backend/src/main/java/com/example/backend/services/authSerivce/UserServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.backend.services;
 import com.example.backend.entity.User;
 import com.example.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,5 +31,12 @@ public class UserServiceImpl implements UserDetailsService {
                 new SimpleGrantedAuthority("ROLE_" + user.getRole().toString())
         );
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
